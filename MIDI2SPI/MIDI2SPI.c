5d1c98d2330ee5173de1c682e45502c5b7071264
Jason Hotchkiss <jason_hotchkiss@hotmail.com>
2014-05-12 22:36:13 +0100

    UPDATES

--- MIDI2SPI/MIDI2SPI.c
+++ MIDI2SPI/MIDI2SPI.c
@@ -23,6 +23,7 @@ typedef unsigned char byte;
 
 #define P_LED		porta.2
 
+volatile byte xx=0;
 ////////////////////////////////////////////////////////////
 // INTERRUPT HANDLER CALLED WHEN CHARACTER RECEIVED AT 
 // SERIAL PORT
@@ -54,6 +55,15 @@ void interrupt( void )
 				P_RUN = 0;
 				break;
 		}				*/
+	}
+	
+	// check if SPI slave interrupt
+	if(pir1.3)
+	{
+		static const char *qq = "HELLO";
+		if(xx++>4) xx=0;
+		ssp1buf=(qq[xx]);		
+		pir1.3=0;
 	}
 }
 
@@ -93,28 +103,63 @@ void init_usart()
 // ENTRY POINT
 void main()
 { 
+	//
+	// CONFIGURE OSCILLATOR
+	//
 	// osc control / 8MHz / internal
 	osccon = 0b01110010;
 	
-	// enable serial receive interrupt
-	intcon = 0b11000000;
-	pie1.5 = 1;
-
-	// configure io
-	trisa = 0b11111011;              	
-	ansela = 0b00000000;
-	porta=0;
-
-	/*
-			VDD-	-VSS
-			 RX-	-SS#
-			SDO-	-SCL
-		   MCLR-	-LED
-	*/
+	//
+	// CONFIGURE PIN FUNCTONS
+	//	1	VDD-		   -VSS 9
+	//	2	 RX-RA5		RA0-SS# 8
+	//	3	SDO-RA4		RA1-SCL 7
+	//	4  MCLR-RA3		RA2-LED	6
+	//
 	apfcon.7=1; // RX on RA5
 	apfcon.6=1; // SDO on RA4
 	apfcon.5=1; // SS# on RA0
 	apfcon.2=1; // TX on RA4
+	
+	//
+	// CONFIGURE IO
+	//
+	trisa.0=1; // SS# as input
+	trisa.1=1; // SCK as input
+	trisa.2=0; // SDI not used (used for LED, set as output)
+	trisa.3=1; // MCLR#
+	trisa.4=0; // SDO as output
+	trisa.5=1; // UART RX as input
+	ansela=0;
+	porta=0;
+	
+	//
+	// CONFIGURE SPI SLAVE
+	//
+	ssp1stat.7 = 0; // SMP must be cleared in SPI slave mode
+	ssp1stat.6 = 0; // CKE clock edge (0=rising, 1=falling)
+	
+	ssp1con1.5 = 1; // SSP1EN enable spi port
+	ssp1con1.4 = 0; // CLP clock polarity (0=idle low, 1=idle high)
+	ssp1con1.3 = 0; // }
+	ssp1con1.2 = 1; // } SSP1M mode 0100 = SPI slave with SS
+	ssp1con1.1 = 0; // }
+	ssp1con1.0 = 0; // }
+	
+	ssp1con3.4 = 0; // BOEN buffer overflow (0=halt, 1=continue)
+
+	//
+	// CONFIGURE INTERRUPTS
+	// 
+	intcon.7 = 1; // GIE
+	intcon.6 = 1; // PEIE
+	pie1.5 = 1;	  // RCIE
+	pie1.3 = 1;	  // SSP1IE
+	
+	//
+	// INIT USART INPUT
+	//
+	init_usart();
 
 	// startup flash
 	P_LED=1; delay_ms(200);
@@ -123,9 +168,6 @@ void main()
 	P_LED=0; delay_ms(200);
 	P_LED=1; delay_ms(200);
 	P_LED=0; delay_ms(200);
-
-	// initialise USART
-	init_usart();
 
 	// loop forever		
 	unsigned long count = 0;
